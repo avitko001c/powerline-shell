ac299d0aec1a0d454831639d0d65d3ed212099fe
Andrew Vitko <Andrew.Vitko@spartasystems.com>
2019-02-28 10:08:20 -0500

    Updated git_url to find command

--- powerline_shell/segments/git_url.py
+++ powerline_shell/segments/git_url.py
@@ -1,22 +1,30 @@
+import os
 import subprocess
 from powerline_shell.utils import RepoStats, ThreadedSegment, get_git_subprocess_env
 from powerline_shell.encoding import get_preferred_output_encoding, get_preferred_input_encoding
 
+try:
+    from shutil import wich  # Python-3.3 and later
+except ImportError:
+    which = lambda f: (lambda fp: os.path.exists(fp) and fp)(os.path.join('/usr/bin', f))
 
 def get_git_url():
-    try:
-        p = subprocess.Popen(['git', 'config', '--get', 'remote.origin.url'],
+    if which('git'):
+        try:
+            p = subprocess.Popen(['git', 'config', '--get', 'remote.origin.url'],
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                              env=get_git_subprocess_env())
-    except OSError:
-        # Popen will throw an OSError if git is not found
-        return 0
-
-    pdata = p.communicate()
-    if p.returncode != 0:
+        except OSError:
+            # Popen will throw an OSError if git is not found
+            return 0
+        pdata = p.communicate()
+        if p.returncode != 0:
+            return 'None'
+        return pdata[0].decode(get_preferred_output_encoding()).rstrip('\n')
+    else:
+        eventlog.info('git command not found: cannot use this segment')
         return 'None'
 
-    return pdata[0].decode(get_preferred_output_encoding()).rstrip('\n')
 
 class Segment(ThreadedSegment):
     def run(self):
